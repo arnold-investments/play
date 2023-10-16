edb2c4af7ada018f8cc8ccc98429ed4c555fa776
Johannes Beranek <johberanek@gmail.com>
2023-10-16 10:27:23 +0200

    Re-Add enhancers

--- framework/src/play/plugins/EnhancerPlugin.java
+++ framework/src/play/plugins/EnhancerPlugin.java
@@ -12,7 +12,8 @@ import play.exceptions.UnexpectedException;
 public class EnhancerPlugin extends PlayPlugin {
 
     protected Enhancer[] defaultEnhancers() {
-        return new Enhancer[] { new ContinuationEnhancer(), new SigEnhancer(), new ControllersEnhancer() /*, new LocalvariablesNamesEnhancer() */ };
+        return new Enhancer[] { new PropertiesEnhancer(), new ContinuationEnhancer(), new SigEnhancer(), new ControllersEnhancer(),
+                new MailerEnhancer(), new LocalvariablesNamesEnhancer() };
     }
 
     @Override
